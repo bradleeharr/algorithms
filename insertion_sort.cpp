d8fc82d48d40da380c206d0c90c843c76d92dc39
Bradlee Harrison <bradlee.harrison22@gmail.com>
2023-07-18 23:53:15 -0500

    Insertion Sort and Tests

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -3,9 +3,24 @@
 #include "insertion_sort.h"
 
 int* insertion_sort(int* A, int n) {
-	std::cout << n << std::endl;
-	for (int i = 0; i < n; i++) {
-		
+	for (int i = 1; i < n; i++) {
+		int key = A[i];
+		// Insert A[i] into the sorted subarray A[0:i-1]
+		int j = i - 1;
+		while (j > -1 && A[j] > key) {
+			A[j + 1] = A[j];
+			j = j - 1;
+		}
+		A[j + 1] = key;
+
+	
+		// Debug Logging to see the sorting going on : 
+		if (n < 20) {
+			for (int i = 0; i < n; i++) {
+				std::cout << A[i] << " ";
+			} 		std::cout << "| key: " << key << " | ";
+			std::cout << std::endl;
+		}
 	}
 	return A;
 
--- insertion_sort_test.cpp
+++ insertion_sort_test.cpp
@@ -1,17 +1,101 @@
 #include "pch.h"
 #include "insertion_sort.h"
-
+#include <chrono> 
 using namespace std;
-TEST(TestCaseName, TestName) {
-	//int A[] = { 1,3,5,7,9,10,8,6,4,2,0 };
-	int A[] = { 0,1,2,3,4,5,6,7,8,9,10 };
 
+// Test to make sure that an arbitrary sorting works correctly
+TEST(InsertionSort, TestArbitrarySort) {
+	int A[] = { 1,3,5,7,9,10,8,6,4,2,0 };
+	int A_expected[] = { 0,1,2,3,4,5,6,7,8,9,10 };
 	int n = sizeof(A) / sizeof(A[0]);
 	insertion_sort(A, n);
-
-	int A_expected[] = { 0,1,2,3,4,5,6,7,8,9,10 };
 	for (int i = 0; i < n; i++) {
 		EXPECT_EQ(A[i], A_expected[i]);
 	}
-	EXPECT_TRUE(true);
+}
+
+// Test to make sure that if n < maxsize, it will correctly 
+// keep the last couple elements unchanged 
+// Showing that it is performing insertion sort
+TEST(InsertionSort, TestPartialSort) {
+	int A[] = { 4,4,4,4,1,1,2,3,4,0,0,0,0,0};
+	int A_expected[] = {0,0,0,1,1,2,3,4,4,4,4,4,0,0};
+	int n = sizeof(A) / sizeof(A[0]);
+	insertion_sort(A, n-2);
+	for (int i = 0; i < n; i++) {
+		EXPECT_EQ(A[i], A_expected[i]);
+	}
+}
+
+// Test a single element array, being sorted should return the array
+TEST(InsertionSort, TestSingleElementArray) {
+	int A[] = { 5 };
+	int A_expected[] = { 5 };
+	int n = sizeof(A) / sizeof(A[0]);
+	insertion_sort(A, n);
+	EXPECT_EQ(A[0], A_expected[0]);
+}
+
+// Test sorting with duplicate elements
+TEST(InsertionSort, TestDuplicateElements) {
+	int A[] = { 4,4,4,4,4,4,3,3,5,5,5 };
+	int A_expected[] = { 3,3,4,4,4,4,4,4,5,5,5};
+	int n = sizeof(A) / sizeof(A[0]);
+	insertion_sort(A, n);
+	for (int i = 0; i < n; i++) {
+		EXPECT_EQ(A[i], A_expected[i]);
+	}
+}
+
+// Test sorting with negative numbers
+TEST(InsertionSort, TestNegativeNumbers) {
+	int A[] = { -5, -3, -1, -4, -2 };
+	int A_expected[] = { -5, -4, -3, -2, -1 };
+	int n = sizeof(A) / sizeof(A[0]);
+	insertion_sort(A, n);
+	for (int i = 0; i < n; i++) {
+		EXPECT_EQ(A[i], A_expected[i]);
+	}
+}
+
+// Tests an already sorted array, the "best case" potentially
+TEST(InsertionSort, TestAlreadySortedAscending) {
+	int A[] = { 1, 2, 3, 4, 5 };
+	int A_expected[] = { 1, 2, 3, 4, 5 };
+	int n = sizeof(A) / sizeof(A[0]);
+	insertion_sort(A, n);
+	for (int i = 0; i < n; i++) {
+		EXPECT_EQ(A[i], A_expected[i]);
+	}
+}
+
+// Tests a descending array, the "worst case" potentially
+TEST(InsertionSort, TestSortedDescending) {
+	int A[] = { 5, 4, 3, 2, 1 };
+	int A_expected[] = { 1, 2, 3, 4, 5 };
+	int n = sizeof(A) / sizeof(A[0]);
+	insertion_sort(A, n);
+	for (int i = 0; i < n; i++) {
+		EXPECT_EQ(A[i], A_expected[i]);
+	}
+}
+
+// Test performance of insertion sort
+TEST(InsertionSort, TestPerformance) {
+	const int array_size = 100000;
+	int* A = new int[array_size]; 
+	srand(time(NULL));
+	for (int i = 0; i < array_size; i++) {
+		A[i] = rand() % array_size;
+	}
+
+	auto start = chrono::high_resolution_clock::now();
+	insertion_sort(A, array_size);
+	auto end = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+	cout << "Execution time: " << duration.count()/1000 << " milliseconds" << endl;
+
+	for (int i = 0; i < array_size - 1; i++) {
+		EXPECT_TRUE(A[i] <= A[i + 1]);
+	}
 }
